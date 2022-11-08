7a6d33b3d3df2956448bbd4328913e30c30223db
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-11-08 02:32:49 +0000

    asdf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.scss";
-// import io from "socket.io-client";
+import io from "socket.io-client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useStores } from "./store";
 
@@ -12,15 +12,16 @@ import News from "./pages/news/index.jsx";
 import Signup from "./pages/auth/signup.jsx";
 import Signin from "./pages/auth/signin.jsx";
 
-// const socket = io.connect("http://localhost:3002");
+const socket = io.connect("http://localhost:3002");
 
 function App() {
-  const { app_store, main_store } = useStores();
+  const { chart_store } = useStores();
 
-  // // To get price feed
-  // socket.on("getfeed", (price) => {
-  //   console.log(price);
-  // });
+  // To get price feed
+  socket.on("getfeed", (price) => {
+    chart_store.price_array.push(price)
+    console.log(chart_store.price_array);
+  });
 
   return (
     <div>
