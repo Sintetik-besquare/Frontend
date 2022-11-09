1b6abfab88390d648f08482f43ac7c3c0ecad030
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-11-09 10:00:15 +0000

    so near... yet so far :(

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { observer } from "mobx-react-lite";
 import "./App.scss";
-import io from "socket.io-client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useStores } from "./store";
 
 import Nav from "./components/navbar";
 import Footer from "./components/footer";
@@ -13,32 +11,11 @@ import News from "./pages/news/index.jsx";
 import Signup from "./pages/auth/signup";
 import Signin from "./pages/auth/signin";
 
-import { getHistoricalFeed } from "./services/historical-feed";
-
 const App = () => {
-  const socket = io.connect("http://localhost:3002");
-  const { chart_store } = useStores();
-  const [history, setHistory] = React.useState(chart_store.historical_price);
-
-  // wrap fetch in useEffect watch empty array (triggers on Mount)
-  useEffect(() => {
-    getHistoricalFeed().then(setHistory);
-    // chart_store.historical_price = history
-    chart_store.updateHistory(history)
-  }, []);
-
-  // To get price feed
-  socket.on("getfeed", (price) => {
-    // console.log(price);
-  });
-
   return (
     <div>
       <BrowserRouter>
         <Nav />
-        <h4>useState.history:</h4> {history}
-        <h4>chart_store.historical_price:</h4> {chart_store.historical_price}
-
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/app" element={<Landing />} />
@@ -47,7 +24,6 @@ const App = () => {
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-
         <Footer />
       </BrowserRouter>
     </div>
--- src/pages/trade/components/chart.jsx
+++ src/pages/trade/components/chart.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { observer } from "mobx-react-lite";
+import io from "socket.io-client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useStores } from "../../../store";
+
+import { getHistoricalFeed } from "../../../services/historical-feed";
 
 const Chart = () => {
+  const socket = io.connect("http://localhost:3002");
+  const { chart_store } = useStores();
+  const [history, setHistory] = React.useState(chart_store.historical_price);
+  const [stream, setStream] = React.useState([]);
+
+  // fetch 10 latest stream on Mount from endpoint
+  useEffect(() => {
+    getHistoricalFeed().then(setHistory);
+    chart_store.updateHistory(history);
+  }, []);
+
+  // fetch subsequent emits from socket
+  useEffect(() => {
+    socket.on("getfeed", (price) => {
+      console.log(price);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Chart</h1>
+      <h1 style={{color:'black'}}>Chart</h1>
+      <h4>useState.history:</h4> {history}
+        <h4>chart_store.historical_price:</h4> {chart_store.historical_price}
     </div>
   );
 };
 
-export default Chart;
+export default observer(Chart);
