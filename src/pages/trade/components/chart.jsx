72c9cd6b7380ee0ff1745cc179a9712f5dd9cd54
aaronbrandonicandrew <aaron@besquare.com.my>
2022-11-17 09:46:52 +0800

    reorganize scss

--- src/pages/trade/components/chart.jsx
+++ src/pages/trade/components/chart.jsx
@@ -80,7 +80,7 @@ const LineChart = () => {
   }, []);
 
   return (
-    <div id="chart-container">
+    <div id="chart-container" data-aos="fade-right" data-aos-duration="1000">
       <div id="chart-header">
         <div id="balance-container">
           <div id="balance-amount">
