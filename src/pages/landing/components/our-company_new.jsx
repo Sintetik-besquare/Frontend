a61cd47ef59ad8816db43e9e1c59fba9cceea03f
siyu-besquare <si.yu@besquare.com.my>
2022-11-24 16:35:37 +0800

    refactored homepage

--- src/pages/landing/components/our-company_new.jsx
+++ src/pages/landing/components/our-company_new.jsx
@@ -1,24 +1,27 @@
+import Graph from "../../../assets/graph_grey.png";
 import Crypto2 from "../../../assets/spacemen.png";
 
 function OurCompany() {
   return (
     <div id="company">
-      <div style={{ paddingRight: "3rem" }}>
+      <div className="homepage-image hide-when-small" data-aos="fade-right">
+        <img src={Graph} altpo="Logo" id="graphic_crypto2" />
+      </div>
+      <div className="about-us-container" style={{width:"120vw", padding:"3rem"}}>
         <h1
-          className="homepage-headline aboutus"
-          data-aos="fade-right" data-aos-duration="1000"
+          className="aboutus"
+          data-aos="fade-right"
+          data-aos-duration="1000"
         >
           ABOUT US
         </h1>
+        <h2 className="aboutus-text" data-aos="fade-up">
+            SINTETIK is one of the world's largest online brokers. We offer CFDs
+            and other derivatives on forex, indices, cryptocurrencies,
+            commodities, and synthetics to millions of registered users across
+            the globe.
+        </h2>
       </div>
-      <h2 className="aboutus-text" data-aos="fade-up">
-        <span className="aboutus-text2">
-          SINTETIK is one of the world's largest online brokers. We offer CFDs
-          and other derivatives on forex, indices, cryptocurrencies,
-          commodities, and synthetics to millions of registered users across the
-          globe.
-        </span>
-      </h2>
     </div>
   );
 }
