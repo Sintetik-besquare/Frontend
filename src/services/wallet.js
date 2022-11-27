685aafeaba795376345b11831f46004fad713de5
MoAfiq <108507255+MoAfiq@users.noreply.github.com>
2022-11-28 00:15:37 +0800

    Update wallet.js

--- src/services/wallet.js
+++ src/services/wallet.js
@@ -1,4 +1,4 @@
-const ENDPOINT_BASE = "http://localhost:3001";
+const ENDPOINT_BASE = "http://login.sintetik.xyz:3001";
 
 async function getBalance() {
   return await fetch(`${ENDPOINT_BASE}/account/getBalance`, {
