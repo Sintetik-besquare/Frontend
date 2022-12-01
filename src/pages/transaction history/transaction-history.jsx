6fd85feac607dd7aad438d9d06c6a31fa7942ca2
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-12-01 09:15:37 +0000

    filter by transaction type

--- src/pages/transaction history/transaction-history.jsx
+++ src/pages/transaction history/transaction-history.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 
 function TransactionHistory() {
   const [filter, setFilter] = useState("All");
+  const [search, setSearch] = useState("All");
   const [transaction, setTransaction] = useState([]);
 
   useEffect(() => {
@@ -21,28 +22,43 @@ function TransactionHistory() {
           <th>transaction_amount</th>
           <th>balance</th>
           <th>transaction_time</th>
-          <th>transaction_type</th>
+          <th className="dropdown-filter"> 
+              <div className="filter">transaction_type<FaFilter/></div>
+              <div className="filter-types">
+                <button onClick={()=>{setFilter('All')}}>All</button>
+                <button onClick={()=>{setFilter("ResetBalance")}}>Reset</button>
+                <button onClick={()=>{setFilter('Buy')}}>Buy</button>
+                <button onClick={()=>{setFilter('Sell')}}>Sell</button>
+              </div>
+          </th>
         </tr>
         {transaction.map((t, i) => {
-          return (
-            <tr key={i}>
-              <td>{t.contract_id}</td>
-              <td>{t.transaction_id}</td>
-              <td>{t.transaction_amount}</td>
-              <td>{t.balance}</td>
-              <td>
-                {new Intl.DateTimeFormat("en-US", {
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  second: "2-digit",
-                }).format(t.transaction_time * 1000)}
-              </td>
-              <td>{t.transaction_type}</td>
-            </tr>
-          );
+
+       if (t.transaction_type === filter ||(filter === "All" && transaction.length !== 0)) {
+         return (
+           <tr key={i}>
+             <td>{t.contract_id}</td>
+             <td>{t.transaction_id}</td>
+             <td>{t.transaction_amount}</td>
+             <td>{t.balance}</td>
+             <td>
+               {new Intl.DateTimeFormat("en-US", {
+                 year: "numeric",
+                 month: "2-digit",
+                 day: "2-digit",
+                 hour: "2-digit",
+                 minute: "2-digit",
+                 second: "2-digit",
+               }).format(t.transaction_time * 1000)}
+             </td>
+             <td>{t.transaction_type}</td>
+           </tr>
+         );
+      } else {
+        return (<></>)
+      }
+
+
         })}
       </table>
     </>
