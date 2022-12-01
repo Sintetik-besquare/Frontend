d1edd66464c45aa9ba306c0072e9fa65de1fa11b
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-12-01 09:58:34 +0000

    change back to localhost

--- src/pages/trade/components/order-form.jsx
+++ src/pages/trade/components/order-form.jsx
@@ -26,7 +26,7 @@ const OrderForm = () => {
   let error_message = [];
 
   useEffect(() => {
-    socket.current = io("https://login.sintetik.xyz", {
+    socket.current = io("http://localhost:3001", {
       query: {
         token: TOKEN,
       },
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-const ENDPOINT_BASE = "https://login.sintetik.xyz";
+const ENDPOINT_BASE = "http://localhost:3001";
 function useLoader(provider, initial = null, deps = []) {
   const [value, setValue] = React.useState(initial);
   useEffect(
