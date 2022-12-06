d08b46e720a2071d58709130105004f04fb4a79c
MoAfiq <108507255+MoAfiq@users.noreply.github.com>
2022-12-06 16:37:55 +0800

    Update news-vertical.jsx

--- src/pages/news/components/news-vertical.jsx
+++ src/pages/news/components/news-vertical.jsx
@@ -26,50 +26,58 @@ const NewsList = () => {
 
   if (!articles && !topheadlines) return <h1>Loading..</h1>;
 
-  return (
-    <>
-      <div className="news-container">
-        <div className="news-container-grid">
-          {topheadlines
-            ?.slice()
-            .slice(0, 1)
-            .map((topheadline, i) => (
-              <HorizontalNewsItem
-                title={topheadline.name}
-                date={topheadline.datePublished}
-                description={topheadline.description}
-                url={topheadline.url}
-                urlToImage={topheadline.image.thumbnail.contentUrl}
-                key={i}
-              />
-            ))}
+  if (!articles && topheadlines) {
+    return <h1 style={{ textAlign: "center" }}>Loading</h1>;
+  } else {
+    return (
+      <>
+        <div className="news-container">
+          <div className="news-container-grid">
+            {topheadlines
+              ?.slice()
+              .slice(0, 1)
+              .map((topheadline, i) => (
+                <HorizontalNewsItem
+                  title={topheadline.name}
+                  date={topheadline.datePublished}
+                  description={topheadline.description}
+                  url={topheadline.url}
+                  urlToImage={topheadline.image.thumbnail.contentUrl}
+                  key={i}
+                />
+              ))}
+          </div>
         </div>
-      </div>
-      <div className="news-container">
-        <div className="news-container-grid">
-          {articles
-            .slice(2)
-            .sort(function (a, b) {
-              return new Date(b.publishedAt) - new Date(a.publishedAt);
-            })
-            .slice(0, cardsAvailable)
-            .map((article, i) => (
-              <NewsItem
-                title={article.name}
-                date={article.datePublished}
-                description={article.description}
-                url={article.url}
-                urlToImage={article.image?.thumbnail?.contentUrl}
-                key={i}
-              />
-            ))}
+        <div className="news-container">
+          <div className="news-container-grid">
+            {articles
+              .slice(2)
+              .sort(function (a, b) {
+                return new Date(b.publishedAt) - new Date(a.publishedAt);
+              })
+              .slice(0, cardsAvailable)
+              .map((article, i) => (
+                <NewsItem
+                  title={article.name}
+                  date={article.datePublished}
+                  description={article.description}
+                  url={article.url}
+                  urlToImage={article.image?.thumbnail?.contentUrl}
+                  key={i}
+                />
+              ))}
+          </div>
         </div>
-      </div>
-      <button onClick={showMoreCards} className="news-load-btn button-40">
-        View More
-      </button>
-    </>
-  );
+        {cardsAvailable < 18 ? (
+          <button onClick={showMoreCards} className="news-load-btn button-40">
+            View More
+          </button>
+        ) : (
+          <></>
+        )}
+      </>
+    );
+  }
 };
 
 export default NewsList;
